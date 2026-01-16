8b27d51cd71ca6a925f83ed8133857dd5be7386d
Wiktor Flis <wikflis@gmail.com>
2026-01-16 12:06:05 +0100

    Fix

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from test-opik!")
-
-
-if __name__ == "__main__":
-    main()
--- src/test_opik/__init__.py
+++ src/test_opik/__init__.py
@@ -1,12 +1,32 @@
+import os
+from openai import OpenAI
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-def hello():
-    print("Hello world")
+@app.get("/test_llm")
+async def hello() -> str:
+
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=os.environ.get("ANTHROPIC_API_KEY"),
+        # anthropic openai-compatible api
+        base_url="https://api.anthropic.com/v1",
+    )
+
+    response = await client.responses.create(
+        model="claude-3-haiku-20240307",
+        instructions="You are a coding assistant that talks like a pirate.",
+        input="How do I check if a Python object is an instance of a class?",
+    )
+    output = response.output_text
+    # if output:
+    #    return output
+
+    return "dupa"
 
 
 @app.get("/")
-async def root():
-    return "Hello world"
+async def root() -> str:
+    return "Hello wordld"
